797d10597dc16d8fff42dc0858b6214468e432b4
koko6a <rus.baranovskiy@gmail.com>
2023-08-08 00:06:07 +0300

    create start module

--- BombGame/Base/BaseViewController.swift
+++ BombGame/Base/BaseViewController.swift
@@ -1,27 +1,27 @@
 import UIKit
 
 extension BaseViewController {
-    struct Constants {
-        let startBackColor: UIColor = BombColor.yellow.color
-        let endColor: UIColor = BombColor.orange.color
+    struct BaseConstants {
+        let yellowColor: UIColor = BombColor.yellow.color
+        let orangeColor: UIColor = BombColor.orange.color
+        let violetColor: UIColor = BombColor.violet.color
     }
 }
 
 class BaseViewController: UIViewController {
     
-    private let constants: Constants
+    private let constants: BaseConstants
     
     private lazy var backgroundLayer: CAGradientLayer = {
         let layer = CAGradientLayer()
-        layer.colors = [constants.startBackColor.cgColor, constants.endColor.cgColor]
-        //layer.locations = [0.0, 1.0]
+        layer.colors = [constants.yellowColor.cgColor, constants.orangeColor.cgColor]
         layer.startPoint = CGPoint(x: 0.0, y: 0.0)
         layer.endPoint = CGPoint(x: 0.0, y: 1.0)
         return layer
     }()
     
     init() {
-        self.constants = Constants()
+        self.constants = BaseConstants()
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -39,4 +39,36 @@ class BaseViewController: UIViewController {
         self.view.layer.insertSublayer(backgroundLayer, at: 0)
         self.backgroundLayer.frame = self.view.bounds
     }
+}
+
+extension BaseViewController {
+    func createButton(title: String, font: UIFont) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(constants.yellowColor, for: .normal)
+        button.backgroundColor = constants.violetColor
+        button.titleLabel?.font = font
+        return button
+    }
+    
+    func createLabel(text: String, font: UIFont, textColor: UIColor, alignment: NSTextAlignment) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.textColor = textColor
+        label.font = font
+        label.textAlignment = alignment
+        return label
+    }
+    
+    func toStackView(
+        subviews: [UIView], spacing: CGFloat,
+        axis: NSLayoutConstraint.Axis, distribution: UIStackView.Distribution,
+        alignment: UIStackView.Alignment) -> UIStackView{
+            let stack = UIStackView(arrangedSubviews: subviews)
+            stack.spacing = spacing
+            stack.axis = axis
+            stack.distribution = distribution
+            stack.alignment = alignment
+            return stack
+        }
 }

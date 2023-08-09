bab6ad4468501e8374d0905a2b903a791c7c158d
Demogorgonius <51359795+Demogorgonius@users.noreply.github.com>
2023-08-09 21:30:56 +0300

    model

---
+++ BombGame/Base/Questions.swift
@@ -0,0 +1,24 @@
+//
+//  Questions.swift
+//  BombGame
+//
+//  Created by Sergey on 09.08.2023.
+//
+
+import Foundation
+
+
+struct Question {
+    var question: [String]
+    var category: String
+}
+
+struct QuestionArray {
+    
+    var qArray: [String: Question]
+    
+}
+
+var questions = Question(question: ["1", "2", "3"], category: "numbers")
+var qArray = 
+
